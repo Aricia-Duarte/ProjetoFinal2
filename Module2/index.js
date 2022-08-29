07efd4e1cf595ce87f68009ce66080d527b0ee56
DamiaoN <dnerivan@icloud.com>
2022-08-29 16:01:45 -0300

    Alt

--- Module2/index.js
+++ Module2/index.js
@@ -1,67 +1,74 @@
-//Configuração inicial 
+//Configuração inicial - OK
 import express from "express" 
 import { appendFile } from "fs";
 import mongoose from "mongoose"
-const server = express (); //variável chamando o express ncomo  uma função
+const server = express (); //variável chamando o express ncomo uma função
 
-//inicar a leitura JSON / criar middlewares
-server.use (
+import BancoSchema from './models/BancoSchema.js'
+
+
+server.use (express.json()); //criando outra middlewares - resposta de volta do JSON
+
+//inicar a leitura JSON / criar middlewares - OK
+server.use ( 
    express.urlencoded({
       extended: true,
      })
 ) 
 
-//rotas da API
-server.post ('/', async (req, res) => {
+//rotas da API - TESTAR
+server.post ('/product', async (req, res) => {
 
-   //req.body
-   const {codigo, descricao, quantidade, preco, imagem } = req.body
+   // requisição do body
+   const {codigo, descricao, preco} = req.body
+   
    if (!codigo) {
-      res.status(422).json ({error:'produto não casdastrado!'})
+      res.status(422).json({error:'produto não casdastrado!'})
    }
    
-   const products = {codigo, descricao, quantidade, preco, imagem }
+   const product = {codigo, descricao, preco}
    
    //Erro 
    try { 
       //Criando os dados
-      await BancoSchema.create(products)
+      await BancoSchema.create(product)
 
-      res.status(500).json({message:'produto inserido com suceso!'})
+      res.status(201).json({message:'produto inserido com suceso!'})
 
-   }  catch (error) {
-      res.status(500).json ({error: error})
+   }  catch(error){
+      res.status(500).json({error: error})
    }
-   
-   //Create
+  
 })
 
-server.use (express.json()); //criando outra middlewares - resposta de volta do JSON
+server.get ('/product', async (req, res) => {
+   res.status(200).json(product)
+})
 
-//rota inicial / endpoint 
+//rota inicial / endpoint 0K
 server.get ('/', (req, res) => { //função callback
    
    //mostrar requisição
-
    return res.json ({message:'teste'})
-
 }) 
 
+
+//Função busca de produtos - TESTAR
+//function buscaCelular (codigo) {
+//    return product.findIndex(celular => celular.codigo == codigo)
+//}
+
+
 //PROMESSAS - Conexão com o Banco de dados
-
-//Login e Senha do Banco de Dados
-
+//Login e Senha do Banco de Dados - OK
 const DB_USER = 'startech'
 const DB_PASSWORD = encodeURIComponent('grupo5')
-server.listen (3600)
+server.listen (3000)
+
 //Acesso ao banco
-
-
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.dz3q6eu.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => {
-   
    console.log('Conectado ao Banco de Dados - MongoBD')
 })
-
 .catch((err) => console.log(err))
