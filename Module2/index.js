bca76e535862d553818b108593100b74477032a5
pauloestevaosh <pauloestevaosh@hotmail.com>
2022-09-04 14:44:26 -0300

    Read adicionado

--- Module2/index.js
+++ Module2/index.js
@@ -1,4 +1,4 @@
-//Configuração inicial 
+//Configuração inicial - OK
 import express from "express" 
 import { appendFile } from "fs";
 import mongoose from "mongoose"
@@ -6,8 +6,8 @@ const server = express (); //variável chamando o express ncomo uma função
 
 import BancoSchema from './models/BancoSchema.js'
 
-//inicar a leitura JSON / criar middlewares
-server.use (
+//inicar a leitura JSON / criar middlewares - OK
+server.use ( 
    express.urlencoded({
       extended: true,
      })
@@ -20,18 +20,18 @@ server.get('/', (req, res) => {
    res.json({ message: 'Bem vindo a nossa API!'})
 })
 
-//rotas da API
-//criaçao de dados
-server.post ('/products', async (req, res) => {
+//Criação de dados
+server.post ('/product', async (req, res) => {
 
    // requisição do body / = 
     const {descricao, preco} = req.body 
 
-   //processo de validacao para garantir de seja enserido um dado valido
+   //processo de validacao para garantir de seja inserido um dado valido
    if (!descricao) {
       res.status(422).json({error:'A descricao e obrigatoria!'})
+      return
    }
-   
+
    const product = {
       descricao,
       preco
@@ -49,23 +49,37 @@ server.post ('/products', async (req, res) => {
   
 })
 
-
-
-server.get ('/products', async (req, res) => {
-
+//Read - Leitura de dados
+server.get('/product', async (req, res) => {
    try {
-      const products = await BancoSchema.find()
+      const dados = await BancoSchema.find()
+      res.status(200).json(dados)
 
    } catch (error) {
       res.status(500).json({error: error})
    }
+})
 
-}) 
+server.get('/product/:id', async (req, res) => {
+   //estrair dados da requisição pela url = req.params
+   const id = req.params.id
+   try {
+      const product = await BancoSchema.findOne({_id: id})
+
+      if(!product) {
+         res.status(422).json({message: 'Produto não encontrado!'})
+         return
+      }
+      res.status(200).json(product)
+   } catch (error) {
+      res.status(500).json({error: error})
+   }
+})
+
+
 
 //PROMESSAS - Conexão com o Banco de dados
-
-//Login e Senha do Banco de Dados
-
+//Login e Senha do Banco de Dados - OK
 const DB_USER = 'startech'
 const DB_PASSWORD = encodeURIComponent('grupo5')
 server.listen (3000)
@@ -74,8 +88,6 @@ server.listen (3000)
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.dz3q6eu.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => {
-   
    console.log('Conectado ao Banco de Dados - MongoBD')
 })
-
 .catch((err) => console.log(err))
